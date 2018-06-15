4282e03fa49ede7c18c63699196e5480c7066337
Slashbot <slashbot@github.com>
2018-06-15 10:30:26 +0000

    Initial commit

---
+++ app/src/main/java/org/a0x00sec/amunet/MainActivity.java
@@ -0,0 +1,69 @@
+package org.a0x00sec.amunet;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                collect_installed_apps();
+            }
+        }).start();
+    }
+
+    private void collect_installed_apps() {
+        final PackageManager pm = getPackageManager();
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        for (ApplicationInfo packageInfo : packages) {
+            if(pm.getLaunchIntentForPackage(packageInfo.packageName) != null)
+            {
+                String app_name = packageInfo.loadLabel(getPackageManager()).toString();
+                String app_package = packageInfo.processName;
+
+                Log.i("0x00sec", "App name: " + app_name + " Package Name: " + app_package);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
